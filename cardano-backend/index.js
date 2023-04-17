61f187ecb342f7d1ea089c237479525edfd27d23
Elarbi RIKAB <elarbi.rikab@etu.univ-lehavre.fr>
2023-04-17 02:55:41 +0200

    ajout partie backend send file (hash + sendBlockChain)

--- cardano-backend/index.js
+++ cardano-backend/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const multer = require('multer'); // middleware pour gérer les fichiers
 const cors = require('cors'); // middleware pour ajouter les entêtes CORS
 const app = express();
-const { Seed, WalletServer } = require('cardano-wallet-js');
+const { Seed, WalletServer, AddressWallet} = require('cardano-wallet-js');
 const {response} = require("express");
+const crypto = require("crypto");
+const fs = require("fs");
+const admin = require('firebase-admin');
+const serviceAccount = require("./key.json");
+
+// Initialisez Firebase Admin SDK avec vos identifiants Firebase
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+const db = admin.firestore();
+
 
 // Middleware pour gérer les fichiers envoyés
 const upload = multer({ dest: 'uploads/' });
@@ -30,6 +41,22 @@ async function createOrRestoreWallet(name,passphrase, mnemonic){
         let mnemonic_sentence = Seed.toMnemonicList(mnemonic);
         let wallet = await walletServer.createOrRestoreShelleyWallet(name,mnemonic_sentence ,passphrase);
 
+        // Creation
+
+        const data = {
+            walletId : wallet.id,
+            walletName: wallet.name,
+            walletState: wallet.state.status,
+            walletAvailableBalance: wallet.getAvailableBalance(),
+            walletRewardBalance : wallet.getRewardBalance(),
+            walletTotalBalance : wallet.getTotalBalance(),
+            walletAddress: await wallet.getAddressAt(0),
+            privateKey: privateKey.to_bech32(),
+            accountKey: accountKey.to_bech32(),
+        }
+        return data;
+
+
     }catch (e) {
         if (e.response.status === 409) {
             //let walletServer = WalletServer.init('http://localhost:8090/v2');
@@ -39,17 +66,20 @@ async function createOrRestoreWallet(name,passphrase, mnemonic){
             const idWallet = extractWalletId(e.response.data.message);
             let wallet = await walletServer.getShelleyWallet(idWallet);
 
+            //console.log(wallet);
+
 
             // KEY HANDLING
             let rootKey = Seed.deriveRootKey(mnemonic);
             let privateKey = Seed.deriveKey(rootKey, ['1852H','1815H','0H','0','0']).to_raw_key();
-            console.log("private key : " + privateKey.to_bech32());
+            //console.log("private key : " + privateKey.to_bech32());
 
 
             let accountKey = Seed.deriveAccountKey(rootKey, 0);
-            console.log("account key : " +  accountKey.to_bech32());
+            //console.log("account key : " +  accountKey.to_bech32());
 
             const data = {
+                walletId : wallet.id,
                 walletName: wallet.name,
                 walletState: wallet.state.status,
                 walletAvailableBalance: wallet.getAvailableBalance(),
@@ -76,10 +106,6 @@ async function createOrRestoreWallet(name,passphrase, mnemonic){
 }
 
 
-
-
-
-
 // Route pour gérer la connexion au portefeuille
 app.post('/walletCli', (req, res) => {
     const {name,passphrase, mnemonic } = req.body;
@@ -98,6 +124,111 @@ app.post('/walletCli', (req, res) => {
             res.status(500).send({ status: 505 , message: `Erreur lors de la création ou de la restauration du portefeuille: ${error}` });
         });
 });
+
+
+
+app.post('/send-file', upload.single('file'), async (req, res) => {
+    const email = req.body.email;
+    const walletId = req.body.walletId;
+    const walletPassphrase = req.body.passphrase;
+    const file = req.file;
+
+
+    // ETAPE 1 HASHAGE DU DOCUMENT
+    // Calculer le hash du fichier en utilisant la bibliothèque crypto
+    const fileStream = fs.createReadStream(file.path);
+    // Étape 1 : calculer le hash du document
+    const hash = crypto.createHash('sha256');
+    fileStream.pipe(hash);
+    const calculatedHash = await new Promise((resolve, reject) => {
+        hash.on('error', reject);
+        hash.on('finish', () => {
+            resolve(hash.digest('hex'));
+        });
+    });
+    fileHash = calculatedHash;
+    console.log('Le hash du fichier est : ' + fileHash);
+
+
+
+    // ETAPE 2 : ENVOIE A LA BLOCKCHAIN
+
+    // 2.A Récupérez l'adresse de portefeuille associée au Destinateur dans la collection "users"
+    const infoDestinataire = await getInfoDestinataireFromEmail(email);
+    console.log("address Wallet Dest : " + infoDestinataire.addressDest);
+    console.log("pk Dest :" + infoDestinataire.pkDest);
+
+    // 2.B Send to blockchain
+
+
+    const senderWallet = await walletServer.getShelleyWallet(walletId);
+    const metadata = {
+        1: {
+            "hash": fileHash,
+        }
+
+    };
+
+    //let receiverAddress = [new AddressWallet(infoDestinataire.addressDest)];
+    //const amounts = [1000000]; // ADA
+    //let transaction = await senderWallet.sendPayment(walletPassphrase, receiverAddress, amounts, metadata);
+    //let transaction = await senderWallet.getTransaction('af0465f610af989dd899a5b6142033dd8f2d3fd754e7799356e70183bbef10e1');
+    console.log(transaction.metadata); //af0465f610af989dd899a5b6142033dd8f2d3fd754e7799356e70183bbef10e1
+
+
+    // Chiffrer le fichier avec la clé publique du destinataire
+    const publicKey = infoDestinataire.pkDest; // La clé publique du destinataire
+    const fileData = fs.readFileSync(file.path);
+    const encodedPublicKey = CardanoCrypto.PublicKey.fromBuffer(Buffer.from(publicKey, 'hex')).toX509SubjectPublicKeyInfo();
+    const encryptedFile = crypto.publicEncrypt(encodedPublicKey, fileData);
+
+    // Enregistrer le fichier chiffré dans IPFS
+    const { cid } = await ipfs.add(encryptedFile);
+
+    console.log(cid);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+async function getInfoDestinataireFromEmail(email){
+    const userRecord = await admin.auth().getUserByEmail(email);
+    const usersRef = admin.firestore().collection('users');
+    const query = usersRef.where('email', '==', userRecord.email).limit(1);
+    const userDocs = await query.get();
+    if (userDocs.empty) {
+        console.log('Aucun utilisateur trouvé avec l\'email spécifié');
+        return null;
+    } else {
+        const userDoc = userDocs.docs[0];
+        const dataDest = {
+            addressDest: userDoc.get('walletAddress'),
+            pkDest : userDoc.get('publicKey'),
+        }
+        //console.log(dataDest);
+        //console.log(`Adresse de portefeuille de destinataire : ${dataDest.addressDest}`);
+        //console.log(`Clé de public de destinataire : ${userDoc.get(dataDest.pkDest)}`);
+        return dataDest;
+    }
+}
+
+
+
 
 
 app.listen(3002, () => {
