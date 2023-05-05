94b4f5faf46c662487abdebe03310f719fc6e4a2
Elarbi RIKAB <elarbi.rikab@etu.univ-lehavre.fr>
2023-05-05 02:36:04 +0200

    Add Page History for received Files

---
+++ cardano-front/src/pages/HistoryReceived.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Box, ChakraProvider, Container} from '@chakra-ui/react';
+import FileReceivedHistory from "../components/Historique/FileRecievedHistory.jsx";
+
+const HistoryReceived = () => {
+
+    return (
+
+
+        <Box  >
+            <FileReceivedHistory  />
+        </Box>
+
+
+    );
+};
+
+export default HistoryReceived;
