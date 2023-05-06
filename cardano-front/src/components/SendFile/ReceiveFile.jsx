83c714a31708a857385f53be182097ee8364ef30
Elarbi RIKAB <elarbi.rikab@etu.univ-lehavre.fr>
2023-05-06 15:23:20 +0200

    fix Recieve File

--- cardano-front/src/components/SendFile/ReceiveFile.jsx
+++ cardano-front/src/components/SendFile/ReceiveFile.jsx
@@ -2,7 +2,8 @@ import {useParams, useSearchParams} from "react-router-dom";
 import {Box, Button, Center, Spinner, Heading, Link, Text, useToast} from "@chakra-ui/react";
 import axios from "axios";
 import React, {useEffect, useRef, useState} from "react";
-
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import {doc, getDoc} from "firebase/firestore";
 
 const FileReceiver = () => {
 
@@ -12,12 +13,15 @@ const FileReceiver = () => {
 
     const cid = searchParams.get("Cid");
     const tx = searchParams.get("tx");
-    const uuid = searchParams.get('uuid');
+    const uuidSender = searchParams.get('uuid');
     const originaName = searchParams.get('fileName');
     const [downloadLink, setDownloadLink] = useState('');
     const hiddenDownloadLink = useRef(null);
     const toast = useToast();
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [user, setUser] = useState(null);
+
+
     // Add a useEffect hook to watch for downloadLink changes
     useEffect(() => {
         // Trigger a click event on the hidden anchor tag only if the download link is set
@@ -27,18 +31,29 @@ const FileReceiver = () => {
         }
     }, [downloadLink]);
 
+    useEffect(() => {
+        const auth = getAuth();
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+            setIsLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
 
     console.log(cid);
     console.log(tx);
 
     const handleFileReceive = () => {
         setIsLoading(true);
-        axios.post('http://localhost:3002/receive-file2', { cid,tx, uuid, originaName,})
+        axios.post('http://localhost:3002/receive-file2', { cid,tx, uuid: user.uid, originaName,})
             .then(response => {
                 setDownloadLink(`http://localhost:3002/download/${response.data.fileName}`);
-
-
-
                 console.log(downloadLink);
 
                 if (response.data.compare){
@@ -79,27 +94,47 @@ const FileReceiver = () => {
             <Text textAlign="center" mb={4}>
                 Description du composant de réception du fichier.
             </Text>
-            <Text textAlign="center" mb={4}>
-                CID : {cid}
-            </Text>
-            <Text textAlign="center" mb={4}>
-                Transaction ID : {tx}
-            </Text>
-            <Text textAlign="center" mb={4}>
-                Sender ID : {uuid}
-            </Text>
-            {/* Add the hidden anchor tag */}
-            <a href={downloadLink} ref={hiddenDownloadLink} download style={{ display: 'none' }}></a>
 
-            <Center>
+            {isLoading && <Spinner
+                thickness='4px'
+                speed='0.65s'
+                emptyColor='gray.200'
+                color='blue.500'
+                size='xl'
+            />}
+
+
+            {user && (<>
+                <Text textAlign="center" mb={4}>
+                You : {user.email}
+                 </Text>
+
+                <Text textAlign="center" mb={4}>
+                CID : {cid}
+                </Text>
+
+                <Text textAlign="center" mb={4}>
+                Transaction ID : {tx}
+                </Text>
+                <Text textAlign="center" mb={4}>
+                Sender ID : {uuidSender}
+                </Text>
+            {/* Add the hidden anchor tag */}
+                <a href={downloadLink} ref={hiddenDownloadLink} download style={{ display: 'none' }}></a>
+
+                <Center>
                 <Button onClick={handleFileReceive} colorScheme="blue">
-                    {isLoading ? (
-                        <Spinner size="sm" mr="2" />
-                    ) : (
-                    "Recevoir un fichier"
-                        )}
+            {isLoading ? (
+                <Spinner size="sm" mr="2" />
+                ) : (
+                "Recevoir un fichier"
+                )}
                 </Button>
-            </Center>
+                </Center>
+            </>)
+            }
+
+
         </Box>
     );
 };
