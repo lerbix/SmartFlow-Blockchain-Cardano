4b367f5a6880d4d38b9e830131e0ba075ed0afbe
Elarbi RIKAB <elarbi.rikab@etu.univ-lehavre.fr>
2023-05-19 16:19:56 +0200

    delete unnecessary imports

--- cardano-front/src/App.jsx
+++ cardano-front/src/App.jsx
@@ -1,18 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import {ChakraProvider, Text} from "@chakra-ui/react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import LogInPage from "./pages/LogInPage.jsx";
 import Dashboard from "./components/DashBoard/DashBoard.jsx";
 import EditProfil from "./components/DashBoard/EditProfil.jsx";
 import WalletConnectorCli from "./components/WalletConnectorCli.jsx";
-import SendFile from "./components/SendFile/SendFile.jsx";
 import ForgetPassword from "./pages/ForgetPassword.jsx";
 import ReceivePage from "./components/SendFile/recieiveFile.jsx";
 import FileReceiver from "./components/SendFile/ReceiveFile.jsx";
-import SendFileForm from "./components/SendFileWallet/SendFileForm.jsx";
-import SendFileDapp from "./components/SendFileWallet/SendFile.jsx";
 import SendFilePage from "./pages/SendFilePage.jsx";
 import HistorySent from "./pages/HistorySent.jsx";
 import HistoryReceived from "./pages/HistoryReceived.jsx";
@@ -20,9 +16,7 @@ import AuthWrapper from "./components/AuthWrapper.jsx";
 import WalletInfo from "./components/WalletInfo.jsx";
 import Check from "./components/DashBoard/check.jsx";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+    return (
 
           <Router>
               <Routes>
