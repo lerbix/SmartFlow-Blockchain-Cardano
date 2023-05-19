ff716f9563f457a97de9efb78f7c29832198ad95
Elarbi RIKAB <elarbi.rikab@etu.univ-lehavre.fr>
2023-05-19 16:23:55 +0200

    delete unnecessary data on HistorySent

--- cardano-front/src/pages/HistorySent.jsx
+++ cardano-front/src/pages/HistorySent.jsx
@@ -3,36 +3,14 @@ import {Box, ChakraProvider, Container} from '@chakra-ui/react';
 import FileSentHistory from "../components/Historique/FileSentHistory.jsx";
 
 const HistorySent = () => {
-    const sampleData = [
-        {
-            senderEmail: 'expediteur@example.com',
-            receiverEmail: 'destinataire@example.com',
-            transactionID: '123abc',
-            ipfsCID: 'QmExAmPlE',
-            dateSent: '2023-05-04 10:00:00',
-            receiptAcknowledged: true,
-        },
-        {
-            senderEmail: 'envoyeur@example.com',
-            receiverEmail: 'recepteur@example.com',
-            transactionID: '456def',
-            ipfsCID: 'QmExAmPlE2',
-            dateSent: '2023-05-04 11:00:00',
-            receiptAcknowledged: false,
-        },
-    ];
+
 
     return (
 
 
         <Box  >
-            <FileSentHistory data={sampleData} />
+            <FileSentHistory />
         </Box>
-
-
-
-
-
 
     );
 };
